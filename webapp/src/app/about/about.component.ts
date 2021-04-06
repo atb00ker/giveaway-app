f6251696903c41a5d42e3b8572316c171d46be41
Ajay Tripathi <ajay39in@gmail.com>
2021-04-07 00:01:28 +0530

    [initial] Home & About Pages added

--- webapp/src/app/about/about.component.ts
+++ webapp/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public contactAuthorText: string = environment.app.contactAuthorText;
   constructor() { }
 
   ngOnInit(): void {
